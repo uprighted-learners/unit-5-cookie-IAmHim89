2420e332dc47e28a0491ce043dad05a0e84587f1
IAmHim89 <computertech0418@gmail.com>
2024-07-10 16:24:29 -0400

    Assignment 98% completed

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
 
 app.use(express.static("public"));
+app.use(cookieParser());
 
 const PORT = 3000;
 
+app.get("/light-mode", (req, res) => {
+  res.cookie("theme", "dark-mode");
+});
+app.get("/dark-mode", (req, res) => {
+  res.cookie("theme", "light-mode");
+});
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
 });
--- public/index.js
+++ public/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+
+let lightMode = document.querySelector("#lightMode");
+let darkMode = document.querySelector("#darkMode");
+
+//lightMode eventListener
+lightMode.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  document.body.className = "light-Mode";
+  fetch("/light-mode");
+});
+//darkmode eventListener
+darkMode.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  document.body.className = "dark-Mode";
+  fetch("/dark-mode");
+});
