4dc9729e2e6dbbac19868bf12a6b828e1009b7cb
IAmHim89 <computertech0418@gmail.com>
2024-07-10 18:56:37 -0400

    assignment Completed

--- index.js
+++ index.js
@@ -8,10 +8,14 @@ app.use(cookieParser());
 const PORT = 3000;
 
 app.get("/light-mode", (req, res) => {
-  res.cookie("theme", "dark-mode");
+  res.cookie("theme", "light-mode");
+  res.sendFile(__dirname + "/public/" + "index.html");
+  //res.redirect("http://localhost:3000/");
 });
 app.get("/dark-mode", (req, res) => {
-  res.cookie("theme", "light-mode");
+  res.cookie("theme", "dark-mode");
+  res.sendFile(__dirname + "/public/" + "index.html");
+  //res.redirect("http://localhost:3000/");
 });
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
--- public/index.js
+++ public/index.js
@@ -1,19 +1,18 @@
 // Your code goes here
+document.body.className = document.cookie.split("=")[1];
 
 let lightMode = document.querySelector("#lightMode");
 let darkMode = document.querySelector("#darkMode");
 
 //lightMode eventListener
-lightMode.addEventListener("click", (e) => {
-  e.preventDefault();
-
-  document.body.className = "light-Mode";
-  fetch("/light-mode");
+lightMode.addEventListener("click", async (e) => {
+  //e.preventDefault();
+  await fetch("/light-mode");
+  document.body.className = document.cookie.split("=")[1];
 });
 //darkmode eventListener
-darkMode.addEventListener("click", (e) => {
-  e.preventDefault();
-
-  document.body.className = "dark-Mode";
-  fetch("/dark-mode");
+darkMode.addEventListener("click", async (e) => {
+  //e.preventDefault();
+  await fetch("/dark-mode");
+  document.body.className = document.cookie.split("=")[1];
 });
